bfdc6f3b6b9734e7e0d60ca8578281d950c347ff
salamander2 <mk_gecko@yahoo.com>
2015-11-15 10:15:51 -0500

    Create ArrayGame.java

---
+++ displayArray/ArrayGame.java
@@ -0,0 +1,10 @@
+package displayArray;
+
+/**
+ * This class must be subclassed in order to display the array graphically.
+ * See "MyArrayGame.java" for a simple example.
+ * See "ArrayDataInterface.java" for method descriptions.
+ * @author Michael Harwood
+ */
+public abstract class ArrayGame implements ArrayDataInterface {	
+}
